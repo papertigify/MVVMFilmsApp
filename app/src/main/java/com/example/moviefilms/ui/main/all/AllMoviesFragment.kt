866f04f3b2c68ab2654343f05d93959b04607e39
Dmitriy <dss111102@gmail.com>
2021-04-27 18:39:30 +0300

    up button setuped, migrated AllFilmsFragment query to Flow

--- app/src/main/java/com/example/moviefilms/ui/main/all/AllMoviesFragment.kt
+++ app/src/main/java/com/example/moviefilms/ui/main/all/AllMoviesFragment.kt
@@ -17,30 +17,51 @@ import com.example.moviefilms.adapters.PagingAdapter
 import com.example.moviefilms.ui.main.MainActivity
 import com.example.moviefilms.ui.viewmodels.MainViewModel
 import com.example.moviefilms.ui.viewmodels.ViewModelProviderFactory
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import dagger.android.support.DaggerFragment
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class AllMoviesFragment: DaggerFragment(R.layout.all_movies_fragment) {
 
     private val TAG = "AllMoviesFragment"
-    private lateinit var viewModel: MainViewModel
+
     private var searchJob: Job? = null
+    private var scrollJob: Job? = null
+
+    private lateinit var viewModel: MainViewModel
     private val mAdapter = PagingAdapter()
     private lateinit var recyclerView: RecyclerView
     private lateinit var progressBar: ProgressBar
+    private lateinit var upButton: FloatingActionButton
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        Log.e(TAG, "Backstack_exp first commit")
         Log.e(TAG, "AllMoviesFragment 1")
         progressBar = view.findViewById(R.id.loadingProgressBar)
-
         recyclerView = view.findViewById(R.id.rvAllMovies)
+        upButton = view.findViewById(R.id.up_button)
+        initRecyclerView()
+        initUpButton()
+
+        viewModel = (activity as MainActivity).viewModel
+
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.allFilms
+                .onEach {
+                    mAdapter.submitData(it)
+                }
+                .collect()
+        }
+    }
+
+    private fun initRecyclerView() {
         recyclerView.apply {
             //layoutManager = GridLayoutManager(activity, 2, GridLayoutManager.VERTICAL, false)
             layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
@@ -48,14 +69,36 @@ class AllMoviesFragment: DaggerFragment(R.layout.all_movies_fragment) {
                 header = MyLoadStateAdapter { mAdapter.retry() },
                 footer = MyLoadStateAdapter { mAdapter.retry() }
             )
+            addOnScrollListener(object : RecyclerView.OnScrollListener(){
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+                    if(dy > 0){
+                        upButton.visibility = View.GONE
+                    }
+                    else if(dy < 0){
+                        upButton.visibility = View.VISIBLE
+                    }
+                }
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+                    if(newState ==  RecyclerView.SCROLL_STATE_IDLE){
+                        scrollJob?.cancel()
+                        scrollJob = viewLifecycleOwner.lifecycleScope.launch {
+                            delay(1700L)
+                            upButton.visibility = View.GONE
+                        }
+                    }
+                }
+            })
         }
-        viewModel = (activity as MainActivity).viewModel
+    }
 
-        // мигрировать на Flow
-        viewModel.allFilms.observe(viewLifecycleOwner){
-            searchJob?.cancel()
-            searchJob = viewLifecycleOwner.lifecycleScope.launch {
-                mAdapter.submitData(it)
+    private fun initUpButton(){
+        upButton.apply {
+            visibility = View.GONE
+            setOnClickListener {
+                recyclerView.scrollToPosition(0)
+                visibility = View.GONE
             }
         }
     }
--- app/src/main/java/com/example/moviefilms/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/moviefilms/ui/viewmodels/MainViewModel.kt
@@ -9,8 +9,7 @@ import androidx.paging.cachedIn
 import com.example.moviefilms.network.FilmListItem
 import com.example.moviefilms.network.MainMoviesApi
 import com.example.moviefilms.repository.MainMoviesRepository
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.*
 //import com.example.moviefilms.repository.MainMoviesRepository
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -19,22 +18,23 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(private val repository: MainMoviesRepository): ViewModel() {
 
 
-    // мигрировать на Flow
-    val allFilms: MutableLiveData<PagingData<FilmListItem>> = MutableLiveData()
+    private val _allFilms = MutableStateFlow(PagingData.empty<FilmListItem>())
+    val allFilms: StateFlow<PagingData<FilmListItem>> = _allFilms.asStateFlow()
 
     init {
         searchAllFilms()
     }
 
-    fun getFilmsListFlow(): Flow<PagingData<FilmListItem>> = repository.getAllMoviesPagerFlow()
+    private fun getFilmsListFlow(): Flow<PagingData<FilmListItem>> = repository.getAllMoviesPagerFlow()
         .cachedIn(viewModelScope)
 
     fun getSearchMoviesFlow(query: String): Flow<PagingData<FilmListItem>> = repository.getSearchMoviesPagerFlow(query)
             .cachedIn(viewModelScope)
 
+
     private fun searchAllFilms() = viewModelScope.launch {
         getFilmsListFlow().collectLatest {
-            allFilms.postValue(it)
+            _allFilms.value = it
         }
     }
 }
